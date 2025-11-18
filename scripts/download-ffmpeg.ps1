83b1a41269638c3a59d47ee398233e5695585342
yeah6161 <barirampage21@gmail.com>
2025-11-19 03:39:48 +0700

    Update download-ffmpeg.ps1

--- scripts/download-ffmpeg.ps1
+++ scripts/download-ffmpeg.ps1
@@ -1,1 +1,17 @@
-# powershell script...
+
+if (!(Get-Command python.exe -ErrorAction SilentlyContinue)) {
+    Write-Host "Python tidak ditemukan. Install dulu sebelum lanjut."
+    exit
+}
+
+# Cek file requirements
+if (!(Test-Path "requirements.txt")) {
+    Write-Host "requirements.txt tidak ditemukan."
+    exit
+}
+
+# Install package
+Write-Host "Menginstall dependencies..."
+python -m pip install --upgrade pip
+python -m pip install -r requirements.txt
+Write-Host "Selesai."
